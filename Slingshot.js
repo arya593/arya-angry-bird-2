de694622bec05c43daf7ccb0dae10b0fa86e76f5
arya593 <79267055+arya593@users.noreply.github.com>
2021-06-11 20:55:11 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,48 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.slingshot = loadImage('angrybirdgameimages/slingshot.png')
+        this.pointB = pointB
+        this.slingshot = Constraint.create(options);
+        World.add(world, this.slingshot);
+    }
+    attach(body){
+        this.slingshot.bodyA = body;
+    }
+    
+    fly(){
+        this.slingshot.bodyA = null;
+    }
+
+    display(){
+        image(this.slingshot,200,20);
+        if(this.slingshot.bodyA){
+            var pointA = this.slingshot.bodyA.position;
+            var pointB = this.pointB;
+            push();
+            
+            stroke(48,22,8);
+            if(pointA.x < 220) {
+                strokeWeight(7);
+                line(pointA.x - 20, pointA.y, pointB.x -10, pointB.y);
+                line(pointA.x - 20, pointA.y, pointB.x + 30, pointB.y - 3);
+                image(this.slingshot,pointA.x -30, pointA.y -10,15,30);
+            }
+            else{
+                strokeWeight(3);
+                line(pointA.x + 25, pointA.y, pointB.x -10, pointB.y);
+                line(pointA.x + 25, pointA.y, pointB.x + 30, pointB.y - 3);
+                image(this.slingshot,pointA.x + 25, pointA.y -10,15,30);
+            }
+           
+            
+            pop();
+        }
+    }
+    
+}
